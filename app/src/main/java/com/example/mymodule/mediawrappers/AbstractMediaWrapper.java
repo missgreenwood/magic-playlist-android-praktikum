894145ea23734e46b58021c38ff5dab6495e40a7
Charlotte Prieß <preiss@cip.ifi.lmu.de>
2014-12-08 14:13:32 +0100

    Einige Funktionen in AbstractMediaWrapper eingefügt, da die ALLE MediaWrapper teilen

--- app/src/main/java/com/example/mymodule/mediawrappers/AbstractMediaWrapper.java
+++ app/src/main/java/com/example/mymodule/mediawrappers/AbstractMediaWrapper.java
@@ -1,13 +1,54 @@
 package com.example.mymodule.mediawrappers;
 
+import android.content.Context;
+
 import com.example.mymodule.mymodule.app.Song;
+
+import java.util.List;
 
 /**
  * Created by lotta on 02.12.14.
  */
 public abstract class AbstractMediaWrapper {
 
+    Context context;
+    private List<Song> songs;
+    private String playPath;
+    private int counter;
+
     public abstract boolean play();
 
+    public abstract boolean lookForSong();
+
+    //TODO: abstract method
+    public abstract void stopPlayer();
+
+    public abstract void pausePlayer();
+
+    public abstract void resumePlayer();
+
+    public Song getSong(int index) {
+        return songs.get(index);
+    }
+
+    public void setSong(List<Song> songs) {
+        this.songs = songs;
+    }
+
+    public String getPlayPath() {
+        return playPath;
+    }
+
+    public void setPlayPath(String playPath) {
+        this.playPath = playPath;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
 
 }
