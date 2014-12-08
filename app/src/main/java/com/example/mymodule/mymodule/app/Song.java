ba4d19fe90ae4a768aa138a24b7472f0c7ddaf6b
Charlotte Prieß <preiss@cip.ifi.lmu.de>
2014-12-08 14:15:38 +0100

    Songs haben einen aktuellen MediaWrapper sowie einen aktuellen MediaWrapperType.
    Ersteres ist wichtig, weil ein Song einen neuen MediaWrapper erhält, wenn der aktuelle den Song nicht abspielen kann

--- app/src/main/java/com/example/mymodule/mymodule/app/Song.java
+++ app/src/main/java/com/example/mymodule/mymodule/app/Song.java
@@ -1,5 +1,7 @@
 package com.example.mymodule.mymodule.app;
 
+import com.example.mymodule.mediawrappers.AbstractMediaWrapper;
+
 /**
  * Created by lotta on 02.12.14.
  */
@@ -10,18 +12,37 @@ public class Song {
 
     //TODO: anderes Package
 
+    public static String MEDIA_WRAPPER_LOCAL_FILE = "local file";
+    public static String MEDIA_WRAPPER_REMOTE_SOUNDCLOUD = "remote file soundcloud";
+    public static String MEDIA_WRAPPER_SPOTIFY = "media warpper spotify";
     //TODO: ändert das so, wie ihrs braucht, also etwas separate Artist-Klasse. Ich werde nur aus dem Song mittels getArtist() den Artist auslesen
     private String artist;
     private String songname;
-
+    private AbstractMediaWrapper mediaWrapper;
+    private String wrapperType;
     public Song(String artist, String songname) {
         this.artist = artist;
         this.songname = songname;
     }
 
+    public AbstractMediaWrapper getMediaWrapper() {
+        return mediaWrapper;
+    }
+
+    public void setMediaWrapper(AbstractMediaWrapper mediaWrapper) {
+        this.mediaWrapper = mediaWrapper;
+    }
+
+    public String getMediaWrapperType() {
+        return wrapperType;
+    }
+
+    public void setMediaWrapperType(String type) {
+        this.wrapperType = type;
+    }
+
 
 //TODO: weitere Metadaten
-
 
     public String getArtist() {
         return artist;
