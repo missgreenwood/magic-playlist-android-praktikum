588456f8adf9ad1e4365cfe107b3f8070fae0f4a
Charlotte Prieß <preiss@cip.ifi.lmu.de>
2014-12-19 13:36:16 +0100

    Package für die Metadatawrappers; wird wohl nur last.fm sein, wobei der lastfmmetadatawrapper die Aufgabe hat mithilfe des APIWrappers API Calls zu machen

---
+++ app/src/main/java/com/example/mymodule/metadatawrappers/AbstractMetadataWrapper.java
@@ -0,0 +1,15 @@
+package com.example.mymodule.metadatawrappers;
+
+import com.example.mymodule.apiwrappers.CallbackInterface;
+
+/**
+ * Created by charlotte on 19.12.14.
+ */
+public class AbstractMetadataWrapper implements CallbackInterface {
+
+
+    @Override
+    public void processWebCallResult(String result, String callback) {
+
+    }
+}
---
+++ app/src/main/java/com/example/mymodule/metadatawrappers/LastfmMetadataWrapper.java
@@ -0,0 +1,12 @@
+package com.example.mymodule.metadatawrappers;
+
+/**
+ * Created by charlotte on 19.12.14.
+ */
+public class LastfmMetadataWrapper extends AbstractMetadataWrapper {
+
+
+    public static final String SIMILAR_ARTISTS_CALLBACK = "similar artists callback";
+
+
+}
