3a937bbede107d33cf74fe0bf6f09321f2e02c0c
Charlotte Prieß <preiss@cip.ifi.lmu.de>
2014-12-20 16:39:00 +0100

    stub für lastfm-Metadaten; ruft die API bereits mit den korrekten Parametern auf in Beispielmethode, Ergebnis wird aber nicht verarbeitet, das ist nicht meine Aufgabe ;-)

--- app/src/main/java/com/example/mymodule/metadatawrappers/AbstractMetadataWrapper.java
+++ app/src/main/java/com/example/mymodule/metadatawrappers/AbstractMetadataWrapper.java
@@ -1,15 +1,15 @@
 package com.example.mymodule.metadatawrappers;
 
+import android.util.Log;
+
 import com.example.mymodule.apiwrappers.CallbackInterface;
 
 /**
  * Created by charlotte on 19.12.14.
  */
-public class AbstractMetadataWrapper implements CallbackInterface {
+public abstract class AbstractMetadataWrapper implements CallbackInterface {
 
 
     @Override
-    public void processWebCallResult(String result, String callback) {
-
-    }
+    public abstract void processWebCallResult(String result, String callback);
 }
