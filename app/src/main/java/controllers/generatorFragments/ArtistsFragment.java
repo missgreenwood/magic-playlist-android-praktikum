b81986b366f7252f73b864087cfb45a056edfe1d
judith <obermaedchen@gmail.com>
2014-12-31 14:40:35 +0100

    basic implementation of artists and songs fragments

--- app/src/main/java/controllers/generatorFragments/ArtistsFragment.java
+++ app/src/main/java/controllers/generatorFragments/ArtistsFragment.java
@@ -2,8 +2,8 @@ package controllers.generatorFragments;
 
 
 import android.app.Activity;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,22 +11,41 @@ import android.view.ViewGroup;
 import tests.R;
 
 /**
- * A simple {@link Fragment} subclass.
+ * Created by judith on 30.12.14.
  */
 public class ArtistsFragment extends Fragment {
 
     private Listener mListener;
+    private OnDataPass dataPasser;
 
     public ArtistsFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_artists, container, false);
+        View view = inflater.inflate(R.layout.fragment_artists, container, false);
+        // Get edittext component
+        /* final EditText editArtist = (EditText) view.findViewById(R.id.editArtist);
+        // Add key listener to keep track of user input
+        editArtist.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    // Display edited artist in toast message
+                    Toast.makeText(getActivity(), editArtist.getText(), Toast.LENGTH_LONG).show();
+                    return true;
+                }
+                return false;
+            }
+        });
+        String editedArtist = editArtist.getText().toString();
+        mListener.artistsClicked(view);
+        // Pass string editedArtist to GeneratorActivity
+        dataPasser.onDataPass(editedArtist); */
+        return view;
     }
 
     @Override
@@ -34,6 +53,7 @@ public class ArtistsFragment extends Fragment {
         super.onAttach(activity);
         try {
             mListener = (Listener) activity;
+            dataPasser = (OnDataPass) activity;
         } catch (ClassCastException e) {
             throw new ClassCastException(activity.toString()
                     + " must implement OnFragmentInteractionListener");
@@ -44,9 +64,16 @@ public class ArtistsFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+        dataPasser = null;
     }
 
     public interface Listener {
         public void artistsClicked(View view);
     }
+
+    // Declare interface to pass string artist to GeneratorActivity
+    public interface OnDataPass {
+        public void onDataPass(String data);
+    }
+
 }
--- app/src/main/java/controllers/generatorFragments/SongsFragment.java
+++ app/src/main/java/controllers/generatorFragments/SongsFragment.java
@@ -3,7 +3,7 @@ package controllers.generatorFragments;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,11 +11,12 @@ import android.view.ViewGroup;
 import tests.R;
 
 /**
- * A simple {@link Fragment} subclass.
+ * Created by judith on 31.12.14.
  */
 public class SongsFragment extends Fragment {
 
     private Listener mListener;
+    private OnDataPass dataPasser;
 
     public SongsFragment() {
         // Required empty public constructor
@@ -34,6 +35,7 @@ public class SongsFragment extends Fragment {
         super.onAttach(activity);
         try {
             mListener = (Listener) activity;
+            dataPasser = (OnDataPass) activity;
         } catch (ClassCastException e) {
             throw new ClassCastException(activity.toString()
                     + " must implement OnFragmentInteractionListener");
@@ -44,10 +46,15 @@ public class SongsFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+        dataPasser = null;
     }
 
     public interface Listener {
         public void songsClicked(View view);
     }
 
+    // Declare interface to pass string song to GeneratorActivity
+    public interface OnDataPass {
+        public void onDataPass(String data);
+    }
 }
