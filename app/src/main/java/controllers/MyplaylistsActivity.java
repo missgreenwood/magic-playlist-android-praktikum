c99b43edd8c41ccf63994a4890de29d0130c584b
judith <obermaedchen@gmail.com>
2015-01-06 17:44:58 +0100

    Add method for button playlists pushed

--- app/src/main/java/controllers/MyplaylistsActivity.java
+++ app/src/main/java/controllers/MyplaylistsActivity.java
@@ -1,39 +1,56 @@
 package controllers;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.View;
 
+import java.util.ArrayList;
+
+import controllers.myplaylistsFragments.PlaylistsListFragment;
+import models.mediaModels.Playlist;
+import models.playlist.PlaylistsManager;
 import tests.R;
 
 /**
  * Created by judith on 27.12.14.
  */
-public class MyplaylistsActivity extends ActionBarActivity {
+public class MyPlaylistsActivity extends ActionBarActivity implements
+    PlaylistsListFragment.Listener {
+
+    private PlaylistsListFragment playlistsListFragment;
+    private TestActivity player;
+    private FragmentManager fragmentManager;
+    private PlaylistsManager manager;
+    private ArrayList<Playlist> myPlaylists;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_myplaylists);
+        fragmentManager = getSupportFragmentManager();
+        manager = new PlaylistsManager();
+        loadPlaylistsListView();
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.myplaylists, menu);
-        return true;
+
+    private void loadPlaylistsListView() {
+        playlistsListFragment = (PlaylistsListFragment) fragmentManager.findFragmentByTag("playlistsListFragment");
+        if (playlistsListFragment == null) {
+            playlistsListFragment = new PlaylistsListFragment();
+            FragmentTransaction transact = getSupportFragmentManager().beginTransaction();
+            transact.add(android.R.id.content, playlistsListFragment, "playlistsListFragment");
+            transact.addToBackStack(null);
+            transact.commit();
+        }
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    public void playlistClicked(View view) {
+        Log.d("", "Klick auf Button");
+        Intent intent = new Intent(this, MediaPlayer.class);
+        startActivity(intent);
     }
-    // TODO: implement class
 }
