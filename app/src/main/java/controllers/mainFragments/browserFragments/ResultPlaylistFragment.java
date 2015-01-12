b470227e431b15106b99c5a3039e5630d57415e4
judith <obermaedchen@gmail.com>
2015-01-12 15:03:58 +0100

    Add basic result playlist fragment + layout

---
+++ app/src/main/java/controllers/mainFragments/browserFragments/ResultPlaylistFragment.java
@@ -0,0 +1,11 @@
+package controllers.mainFragments.browserFragments;
+
+import android.app.Fragment;
+
+/**
+ *
+ */
+public class ResultPlaylistFragment extends Fragment {
+}
+
+// TODO: create view incl. rating button
